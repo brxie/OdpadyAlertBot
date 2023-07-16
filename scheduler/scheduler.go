a596aa4363f346ee75ac29712e877089d2100996
brxie <morpher89@gmail.com>
2023-07-16 08:11:47 +0200

    improve scheduler

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -9,28 +9,29 @@ import (
 	"github.com/brxie/OdpadyAlertBot/config"
 )
 
+const nextTrigCheckBackoff = time.Minute
+
 func Run(region config.Region, events []config.Event, errCh chan error, cb func(e config.Event, scheduledDate time.Time)) {
-	log.Printf("started scheduler for region '%s' with schedule time '%s'", region.ID, region.ScheduleTime)
+	regionSchedTime, err := time.Parse("15:04", region.ScheduleTime)
+	if err != nil {
+		errCh <- fmt.Errorf("unrecoverable scheduler error, can't parse scheduled time, region '%s': %v", region.ID, err)
+		return
+	}
+
 	for {
-		t, err := time.Parse("15:04", region.ScheduleTime)
-		if err != nil {
-			errCh <- fmt.Errorf("unrecoverable scheduler error, can't parse scheduled time, region '%s': %v", region.ID, err)
-			return
-		}
-
 		now := time.Now()
-		todaySchedTime := time.Date(
+		nextSchedCheckTime := time.Date(
 			now.Year(), now.Month(), now.Day(),
-			t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), time.Local)
+			regionSchedTime.Hour(), regionSchedTime.Minute(), regionSchedTime.Second(), regionSchedTime.Nanosecond(), time.Local)
 
-		// if trigger time is from past and older than 1m do not schedule to avoid sending
-		// notification twice after restart
-		if now.Sub(todaySchedTime) > time.Minute {
-			time.Sleep(time.Minute)
-			continue
+		// if trigger time is from past this means it was already sent in this day and
+		// we should wait until next day which is nearest possible day of event
+		if now.Sub(nextSchedCheckTime) > nextTrigCheckBackoff {
+			nextSchedCheckTime = nextSchedCheckTime.Add(time.Hour * 24)
 		}
-		log.Printf("next schedule trigget check for region '%s': '%s'", region.ID, todaySchedTime)
-		time.Sleep(time.Until(todaySchedTime))
+
+		log.Printf("next schedule check for region '%s': '%s'", region.ID, nextSchedCheckTime)
+		time.Sleep(time.Until(nextSchedCheckTime))
 
 		for _, e := range events {
 			eventDate, err := time.Parse("2006-01-02", e.Date)
@@ -39,14 +40,15 @@ func Run(region config.Region, events []config.Event, errCh chan error, cb func(
 				return
 			}
 
-			tommorow := now.AddDate(0, 0, 1)
+			tommorow := time.Now().AddDate(0, 0, 1)
 			if eventDate.Year() == tommorow.Year() &&
 				eventDate.Month() == tommorow.Month() &&
 				eventDate.Day() == tommorow.Day() {
 				log.Printf("sending message for region '%s': %v", region.ID, strings.Join(e.Events, ", "))
-				cb(e, todaySchedTime)
+				cb(e, nextSchedCheckTime)
+				break
 			}
 		}
-		time.Sleep(time.Minute)
+		time.Sleep(nextTrigCheckBackoff)
 	}
 }
